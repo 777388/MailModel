10f9c9935795bf658d6a969f40b8b0b39204d306
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-15 20:55:23 -0700

    Add files via upload

---
+++ MailModel.py
@@ -0,0 +1,7 @@
+import requests
+def send():
+    requests.models.complexjson.scanner.__name__ = __package__.__sizeof__.__self__.__subclasshook__.__call__
+    vin = lambda T: requests.request.__call__(T)
+    t = [].__reversed__()
+    (list(map(vin, t)))
+send()
---
+++ microscope.py
@@ -0,0 +1,17 @@
+try:
+    class intact:
+        def __init__(self, allowance):
+            self.allowance = allowance
+            if (dir.__self__):
+                allowance += self
+            else:
+                allowance += dir.__self__
+        def __init_subclass__(self):
+            import os
+            os.popen(f"echo '{str(dir.__self__)}'")
+            os.popen(f"echo '{str(dir.__self__)}'").read()
+    class excavate(intact):
+        def __init__(self, allowance):
+            super(excavate, self).__init__(allowance)
+except:
+    dir.__self__ = "maneuver"
